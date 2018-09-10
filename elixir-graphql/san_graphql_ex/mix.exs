a821170644540457ba277c9c751b5ec291488716
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-09-10 17:25:55 +0300

    Add Elixir GraphQL API usage example

---
+++ elixir-graphql/san_graphql_ex/mix.exs
@@ -0,0 +1,27 @@
+defmodule SanGraphqlEx.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :san_graphql_ex,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:neuron, "~> 0.8"}
+    ]
+  end
+end
---
+++ elixir-graphql/san_graphql_ex/test/san_graphql_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule SanGraphqlExTest do
+  use ExUnit.Case
+  doctest SanGraphqlEx
+
+  test "greets the world" do
+    assert SanGraphqlEx.hello() == :world
+  end
+end
